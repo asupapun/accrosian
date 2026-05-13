c2bbc327b40b9845a7213f8746748e4b67bcffac
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-05-13 17:10:22 +0530

    changing in css

--- resources/views/frontend/medinfo.blade.php
+++ resources/views/frontend/medinfo.blade.php
@@ -1433,7 +1433,7 @@ h3 {
 
     <!-- HERO -->
     <section class="hero">
-        <img src="{{ asset('assets/images/hero-medinfo.jpeg') }}" alt="Hero Background" class="hero-bg-img" />
+        <img src="{{ asset('assets/images/hero-medin.jpeg') }}" alt="Hero Background" class="hero-bg-img" />
         <div class="hero-bg">
         </div>
         <div class="container">
