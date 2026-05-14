bd043d0a8c539ba7611890734cd15ab07c17d50e
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-05-14 15:06:48 +0530

    changing in css

--- app/Http/Controllers/Frontend/HealthController.php
+++ app/Http/Controllers/Frontend/HealthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Models\Setting;
 
-class BankingController extends Controller
+class HealthController extends Controller
 {
     public function index()
     {
