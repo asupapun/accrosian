4d42776862a2faf3d4a483146531db60ea1d44fa
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-05-18 12:25:49 +0530

    changing in css

--- resources/views/frontend/healthcare.blade.php
+++ resources/views/frontend/healthcare.blade.php
@@ -1527,7 +1527,7 @@ section {
     font-family: 'Sora', sans-serif;
     font-weight: 800;
     font-size: 1.6rem;
-    color: var(--gradient-orange);
+    background: var(--gradient-orange);
     margin-bottom: 4px;
 }
 
