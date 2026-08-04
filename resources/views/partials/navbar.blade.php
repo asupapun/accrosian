f2bfe0b2643d95ce36e27e9811763c55921743ee
Laxmikanta <laxmikanta.accrosian@gmail.com>
2026-08-04 17:51:24 +0530

    changing in css

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -58,7 +58,7 @@
                 <a href="#">Products</a>
                 <div class="simple-dropdown">
                     <a href="{{ route('Products.crm') }}">CRM (Lead Management)</a>
-                    <a href="{{ route('Products.whatsapp') }}">WhatsApp Automation</a>
+                    <a href="{{ route('Products.whtsapp') }}">WhatsApp Automation</a>
                 </div>
             </li>
 
